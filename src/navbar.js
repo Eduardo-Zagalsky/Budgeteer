2d522fd6caa403f4848518a400a5e0a842193a64
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-08-15 01:59:39 -0500

    corrected the links for the app

--- src/navbar.js
+++ src/navbar.js
@@ -4,13 +4,12 @@ import "./navbar.css";
 const NavBar = () => {
     return (
         <nav>
-            <h1 id="title">Shop Budget</h1>
+            <h1 id="title">Budgeteer</h1>
             <NavLink className="nav-link" to="/">Home</NavLink>
             <NavLink className="nav-link" to="/register">Sign Up</NavLink>
-            <NavLink className="nav-link" to="/login">Sign In</NavLink>
+            <NavLink className="nav-link" to="/login">Log In</NavLink>
             <NavLink className="nav-link" to="/logout">Sign out</NavLink>
             <NavLink className="nav-link" to="/budget">Budget</NavLink>
-            <NavLink className="nav-link" to="/shop">Shopping</NavLink>
         </nav>
     );
 }
