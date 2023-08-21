94061fd0eb339f0f3b7c750cf5713522a5d594c3
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-08-20 22:12:13 -0500

    updated the forms with password length

--- src/backend/forms.py
+++ src/backend/forms.py
@@ -1,10 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, PasswordField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Length
 
 
 class AddUserForm(FlaskForm):
     name = StringField("Full Name", validators=[InputRequired()])
     email = EmailField("Email", validators=[InputRequired()])
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired(), Length(
+        min=8, max=20, message="password is either too short or too long")])
+
+
+class LoginForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired()])
     password = PasswordField("Password", validators=[InputRequired()])
