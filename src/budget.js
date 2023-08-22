3416172bda358638dec20c6a32e2fa58f978a268
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-08-22 02:13:49 -0500

    added an outline for what the budget page should consist of

--- src/budget.js
+++ src/budget.js
@@ -1,5 +1,15 @@
 const Budget = (props) => {
     return (
-        <p></p>
+        <div>
+            <p>your income is {income}</p>
+            <p>your expenses are {expenses}</p>
+            <p>ideally you would want your expenses to be less than 45% of your income so let's try to come up with some ways to make that true</p>
+
+            <h1>currently your DTI is {(expenses / income * 100)}</h1>
+            <p>lets see your expenses</p>
+            {/* {for(let x in expenses)} */}
+            <p>looking at this which of these could you cut back on to try to get on the best path to financial freedom?</p>
+
+        </div>
     )
 }
