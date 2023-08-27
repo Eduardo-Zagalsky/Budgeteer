e2b9b2f72933c64e4abbeff4faf0179dfa1eb896
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-08-27 17:18:21 -0500

    hard coded data to make the page function until i connect it to the backend

--- src/budget.js
+++ src/budget.js
@@ -1,15 +1,18 @@
-const Budget = (props) => {
+const Budget = () => {
+    let tempIncome = 2000;
+    let tempExpenses = 950;
     return (
         <div>
-            <p>your income is {income}</p>
-            <p>your expenses are {expenses}</p>
+            <p>your income is {tempIncome}</p>
+            <p>your expenses are {tempExpenses}</p>
             <p>ideally you would want your expenses to be less than 45% of your income so let's try to come up with some ways to make that true</p>
 
-            <h1>currently your DTI is {(expenses / income * 100)}</h1>
+            <h1>currently your DTI is {(tempExpenses / tempIncome * 100)}</h1>
             <p>lets see your expenses</p>
             {/* {for(let x in expenses)} */}
-            <p>looking at this which of these could you cut back on to try to get on the best path to financial freedom?</p>
+            <p>looking at this, which of these could you cut back on to try to get on the best path to financial freedom?</p>
 
         </div>
     )
 }
+export default Budget;
