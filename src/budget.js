9157e7c243b2619506961c7fabc3183aa0756da4
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-08-28 02:55:34 -0500

    moved somethings around to make it more visually satisfying

--- src/budget.js
+++ src/budget.js
@@ -3,15 +3,11 @@ const Budget = () => {
     let tempExpenses = 950;
     return (
         <div>
-            <p>your income is {tempIncome}</p>
-            <p>your expenses are {tempExpenses}</p>
-            <p>ideally you would want your expenses to be less than 45% of your income so let's try to come up with some ways to make that true</p>
-
-            <h1>currently your DTI is {(tempExpenses / tempIncome * 100)}</h1>
-            <p>lets see your expenses</p>
+            <p>Your income is <b>${tempIncome.toLocaleString()}</b> and your expenses are <b>${tempExpenses.toLocaleString()}</b>.</p>
+            <h4>Currently your DTI is {(tempExpenses / tempIncome * 100)}%</h4>
+            <p>Ideally you would want your expenses to be less than 45% of your income, so let's try to come up with some ways to make that true!
+                Let's see your expenses and decide which of these you could cut back on to try to get on the best path to financial freedom.</p>
             {/* {for(let x in expenses)} */}
-            <p>looking at this, which of these could you cut back on to try to get on the best path to financial freedom?</p>
-
         </div>
     )
 }
