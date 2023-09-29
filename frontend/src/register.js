e77e248ea7055c5394fce0ef79dc1b59eebbf0d0
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-09-29 18:55:23 -0500

    removed the useEffect part of the react component, added the jwt,added enviroment variables for a secret key and full url. Added the handleChange function, and turned the submit function to a asynchronous api submit

--- frontend/src/register.js
+++ frontend/src/register.js
@@ -1,45 +1,43 @@
 import axios from "axios"
-import { useState, useEffect } from "react"
-const INITIAL_VAL = { name: "", email: "", username: "", password: "", income: "", creditScore: "" }
+import { useState } from "react"
+const jwt = require("jsonwebtoken");
 
+const INITIAL_VAL = { name: "", email: "", username: "", password: "", income: "", creditScore: "" }
+const URL = process.env.FULL_URL;
+const SECRET_KEY = process.env.SECRET_KEY;
 const Register = () => {
     const [formData, setFormData] = useState(INITIAL_VAL);
-    useEffect(
-        async function signUp() {
-            const data = await axios.post('/signup', { formData });
-            return data;
-        })
-    const handleSubmit = (e) => {
+    const handleChange = (e) => {
+        let { name, value } = e.target;
+        setFormData(data => ({ ...data, [name]: value }));
+    }
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        setFormData(
-            {
-                name: e.target.name.value, email: e.target.email.value, username: e.target.username.value,
-                password: e.target.password.value, income: e.target.income.value, creditScore: e.target.creditScore.value
-            }
-        )
-
+        let token = jwt.sign(formData, SECRET_KEY);
+        const data = await axios.post(`${URL}/signup`, { token });
+        return data.json();
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="name">Full Name: </label>
-                <input type="text" name="name" value={formData.name} />
+                <input type="text" name="name" value={formData.name} onChange={handleChange} />
 
                 <label htmlFor="email">Email: </label>
-                <input type="email" name="email" value={formData.email} />
+                <input type="email" name="email" value={formData.email} onChange={handleChange} />
 
                 <label htmlFor="username">Username: </label>
-                <input type="text" name="username" value={formData.username} />
+                <input type="text" name="username" value={formData.username} onChange={handleChange} />
 
                 <label htmlFor="password">Password: </label>
-                <input type="password" name="password" value={formData.password} />
+                <input type="password" name="password" value={formData.password} onChange={handleChange} />
 
                 <label htmlFor="income">Income: </label>
-                <input type="text" name="income" value={formData.income} />
+                <input type="text" name="income" value={formData.income} onChange={handleChange} />
 
                 <label htmlFor="creditScore">Credit Score: </label>
-                <input type="text" name="creditScore" value={formData.creditScore} />
+                <input type="text" name="creditScore" value={formData.creditScore} onChange={handleChange} />
             </form>
         </div>
     )
