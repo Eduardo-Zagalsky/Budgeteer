64d8a1203f08fc41daaabe4db1f3adf83d604b54
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-10-06 00:24:04 -0500

    incomplete; added the local storage to bring in the token for validation. it should validate the page and if it fails redirect ot the login page, otherwise it should accept the information and add it to the table

--- frontend/src/account-form.js
+++ frontend/src/account-form.js
@@ -1,38 +1,54 @@
 import axios from "axios"
-import { useState, useEffect } from "react"
+import { useState } from "react"
+const local = require("localStorage")
+const URL = process.env.FULL_URL;
+const SECRET = process.env.SECRET_KEY;
 const INITIAL_VAL = { name: "", type: "", balance: "" }
+const jwt = require("jsonwebtoken")
 
 const AccountForm = () => {
     const [formData, setFormData] = useState(INITIAL_VAL);
-    useEffect(
-        async function mark() {
-            const data = await axios.post('/account-form', { formData });
-            return data;
-        })
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setFormData(
-            {
-                name: e.target.name.value, type: e.target.type.value, balance: e.target.balance.value
-            }
-        )
-
+    const [loggedIn, setLoggedIn] = useState(false);
+    const handleChange = (e) => {
+        let { name, value } = e.target;
+        setFormData(data => ({ ...data, [name]: value }));
     }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await axios.post(`${URL}/account-form`, { formData });
+    }
+    const validate = () => {
+        const token = local.getItem("token");
+        const data = jwt.verify(token, SECRET);
+        if (data.username) {
+            setLoggedIn(true);
+        }
+    }
+    if (!loggedIn) {
+        setTimeout(function () {
+            window.location.replace('/login');
+        }, 5000);
+        return (
+            <div>
+                <h1>YOU ARE NOT LOGGED IN! LOG IN AND TRY AGAIN!</h1>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="name">Name: </label>
+                    <input type="text" name="name" value={formData.name} onChange={handleChange} />
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Name: </label>
-                <input type="text" name="name" value={formData.name} />
+                    <label htmlFor="type">Type: </label>
+                    <input type="text" name="type" value={formData.type} onChange={handleChange} />
 
-                <label htmlFor="type">Type: </label>
-                <input type="text" name="type" value={formData.type} />
+                    <label htmlFor="balance">Balance: </label>
+                    <input type="text" name="balance" value={formData.balance} onChange={handleChange} />
 
-                <label htmlFor="balance">Balance: </label>
-                <input type="text" name="balance" value={formData.balance} />
-
-            </form>
-        </div>
-    )
+                </form>
+            </div>
+        )
+    }
 }
 export default AccountForm;
